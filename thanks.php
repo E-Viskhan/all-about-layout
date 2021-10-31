bcfde3d4dd1f119ae4c8eedbb510994a8d3bd362
E-Viskhan <viskhan11@gmail.com>
2021-10-31 05:17:15 +0300

    fix: fixed thanks page

--- thanks.php
+++ thanks.php
@@ -1,1 +1,10 @@
-Пока что пустой файл
+<?php @include('modules/header.php') ?>
+<div class="container thanks">
+  <h1 class="thanks__title">Спасибо за вашу заявку!</h1>
+  <span class="thanks__text">Мы свяжемся с Вами в ближайшее время.</span>
+  <span class="thanks__text">А если у Вас "горит" и ждать нельзя, скорее звоните нам:</span>
+  <a class="thanks__phone" href="tel:8800-100-13-55">8 800 100-13-55</a>
+  <span class="thanks__phone-text">Звонок по России бесплатный!</span>
+</div>
+<!-- /.container -->
+<?php @include('modules/footer.php') ?>
