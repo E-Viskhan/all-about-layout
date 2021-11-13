33d2a05d806a230bed8ba563cce6de173864e992
E-Viskhan <viskhan11@gmail.com>
2021-11-13 12:01:33 +0300

    create: added scroll-up button

--- js/main.js
+++ js/main.js
@@ -133,6 +133,42 @@ $(function () {
 
   // Конец кода для изменения хедера
 
+  // Начало кода для стрелки вверх
+  const offset = 100;
+  const scrollUp = document.querySelector('.scroll-up');
+  const scrollUpSvgPath = document.querySelector('.scroll-up__svg-path');
+  const pathLength = scrollUpSvgPath.getTotalLength();
+
+  scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`
+  scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms'
+
+  const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+  // updateDashOffset
+  const updateDashOffset = () => {
+    const height = document.documentElement.scrollHeight - window.innerHeight
+    const dashoffset = pathLength - (getTop() * pathLength / height);
+
+    scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+  };
+  // onScroll
+  window.addEventListener('scroll', () => {
+    updateDashOffset();
+    if (getTop() > offset) {
+      scrollUp.classList.add('scroll-up-active')
+    } else {
+      scrollUp.classList.remove('scroll-up-active')
+    }
+  });
+  // click
+  scrollUp.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+  // Конец кода для стрелки вверх
+
   // Плавная прокрутка по якорным ссылкам
   var $page = $('html, body');
   $('a[href*="#"]').on("click", function () {
--- modules/footer.php
+++ modules/footer.php
@@ -243,6 +243,20 @@
       <!-- /.modal__dialog -->
     </div>
     <!-- /.modal -->
+    <div class="scroll-up">
+      <svg class="scroll-up__svg" viewBox="-2 -2 52 52">
+        <path
+          class="scroll-up__svg-path"
+          d="
+            M24,0
+            a24,24 0 0,1 0,48
+            a24,24 0 0,1 0,-48
+          "
+        />
+      </svg>
+    </div>
+    <!-- /.scroll-up -->
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=smoothscroll"></script>
     <script src="js/jquery-3.6.0.min.js"></script>
     <script src="js/jquery.validate.min.js"></script>
     <script src="js/slick.min.js"></script>
